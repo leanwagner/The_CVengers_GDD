e9ecfb7f51ad9a63d3be3ef431c14a982c89fdad
Leandro Wagner <leandro.gaston.wagner@gmail.com>
2015-12-03 04:10:43 -0300

    saco row header data grid view

--- AerolineaFrba/AerolineaFrba/Compra/Compra.Designer.cs
+++ AerolineaFrba/AerolineaFrba/Compra/Compra.Designer.cs
@@ -189,6 +189,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(22, 19);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.Size = new System.Drawing.Size(580, 150);
             this.dataGridView1.TabIndex = 0;
             // 
