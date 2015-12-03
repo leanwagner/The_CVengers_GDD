946ec6817027464aa6259cfdbb5b01e9bc0fd872
FrancoVare <vare.franco@gmail.com>
2015-12-03 17:14:25 -0300

    consulta de millas con historial placeholder

--- AerolineaFrba/AerolineaFrba/Consulta Millas/Consulta_Millas.Designer.cs
+++ AerolineaFrba/AerolineaFrba/Consulta Millas/Consulta_Millas.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.dniBox = new System.Windows.Forms.TextBox();
             this.millasLabel = new System.Windows.Forms.Label();
@@ -35,8 +36,14 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label3 = new System.Windows.Forms.Label();
             this.nameBox = new System.Windows.Forms.TextBox();
+            this.errorName = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorDni = new System.Windows.Forms.ErrorProvider(this.components);
+            this.notFound = new System.Windows.Forms.ErrorProvider(this.components);
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorName)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorDni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.notFound)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -59,7 +66,7 @@
             // millasLabel
             // 
             this.millasLabel.AutoSize = true;
-            this.millasLabel.Location = new System.Drawing.Point(173, 22);
+            this.millasLabel.Location = new System.Drawing.Point(188, 22);
             this.millasLabel.Name = "millasLabel";
             this.millasLabel.Size = new System.Drawing.Size(99, 13);
             this.millasLabel.TabIndex = 2;
@@ -68,20 +75,31 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 70);
+            this.groupBox1.Location = new System.Drawing.Point(26, 70);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(260, 200);
+            this.groupBox1.Size = new System.Drawing.Size(228, 200);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Detalle";
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(248, 175);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(216, 175);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // label3
             // 
@@ -98,6 +116,20 @@
             this.nameBox.Name = "nameBox";
             this.nameBox.Size = new System.Drawing.Size(100, 20);
             this.nameBox.TabIndex = 2;
+            this.nameBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // errorName
+            // 
+            this.errorName.ContainerControl = this;
+            // 
+            // errorDni
+            // 
+            this.errorDni.ContainerControl = this;
+            // 
+            // notFound
+            // 
+            this.notFound.ContainerControl = this;
+            this.notFound.RightToLeft = true;
             // 
             // Consulta_Millas
             // 
@@ -111,9 +143,14 @@
             this.Controls.Add(this.dniBox);
             this.Controls.Add(this.label1);
             this.Name = "Consulta_Millas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consulta de millas";
+            this.Load += new System.EventHandler(this.Consulta_Millas_Load);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorName)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorDni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.notFound)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,5 +165,8 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.ErrorProvider errorName;
+        private System.Windows.Forms.ErrorProvider errorDni;
+        private System.Windows.Forms.ErrorProvider notFound;
     }
 }
