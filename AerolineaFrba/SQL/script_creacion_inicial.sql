05f503d03962b3820ab07ac40eb30cb3b8f85215
MiguelArrondo <miguearrondo@hotmail.com>
2015-12-05 01:04:52 -0300

    modificacion proc modificacion ruta

--- AerolineaFrba/SQL/script_creacion_inicial.sql
+++ AerolineaFrba/SQL/script_creacion_inicial.sql
@@ -750,11 +750,9 @@ return
 end
 
 go
-CREATE PROCEDURE THE_CVENGERS.modificacionRuta @P0 as numeric(18,0), @P1 as numeric(18,0), @P2 as nvarchar(100), @P3 as nvarchar(100), @P4 as numeric(18,2), @P5 as numeric(18,2), @P6 as nvarchar(100), @P7 as nvarchar(100), @P8 as nvarchar(100)
+CREATE PROCEDURE THE_CVENGERS.modificacionRuta @P0 as numeric(18,0), @P1 as numeric(18,0), @P4 as numeric(18,2), @P5 as numeric(18,2), @P6 as nvarchar(100), @P7 as nvarchar(100), @P8 as nvarchar(100)
 as 
 begin
-DECLARE @ORIGEN NUMERIC(18,0)
-DECLARE @DESTINO NUMERIC(18,0)
 DECLARE @SERV1 NUMERIC(18,0)
 DECLARE @SERV2 NUMERIC(18,0)
 DECLARE @SERV3 NUMERIC(18,0)
@@ -775,8 +773,6 @@ RAISERROR('La ruta debe tener por lo menos un tipo de servicio asignado.',16,1)
 return
 END 
 
-SET @ORIGEN = (SELECT C.CIUDAD_ID FROM THE_CVENGERS.CIUDAD C WHERE C.CIUDAD_NOMBRE like ('_' + @P2))
-SET @DESTINO = (SELECT C.CIUDAD_ID FROM THE_CVENGERS.CIUDAD C WHERE C.CIUDAD_NOMBRE like ('_' + @P3))
 SET @SERV1 = (SELECT S.SERVICIO_ID FROM THE_CVENGERS.SERVICIO S WHERE S.SERVICIO_NOMBRE = @P6)
 SET @SERV2 = (SELECT S.SERVICIO_ID FROM THE_CVENGERS.SERVICIO S WHERE S.SERVICIO_NOMBRE = @P7)
 SET @SERV3 = (SELECT S.SERVICIO_ID FROM THE_CVENGERS.SERVICIO S WHERE S.SERVICIO_NOMBRE = @P8)
@@ -787,13 +783,8 @@ RAISERROR('No se puede modificar esta ruta debido a que existen viajes en curso 
 return
 END
 
-if(exists(select R.RUTA_ID FROM THE_CVENGERS.RUTA R WHERE R.RUTA_ORIGEN = @ORIGEN AND R.RUTA_DESTINO = @DESTINO AND R.RUTA_ESTADO = 1 and r.RUTA_ID <> @P0))
-BEGIN
-RAISERROR('Ya existe una ruta con ese mismo origen y destino.',16,1)
-return
-end
 
-update THE_CVENGERS.RUTA SET RUTA_CODIGO = @P1, RUTA_ORIGEN = @ORIGEN, RUTA_DESTINO = @DESTINO, RUTA_PRECIO_BASE_POR_KILO = @P4, RUTA_PRECIO_BASE_POR_PASAJE = @P5
+update THE_CVENGERS.RUTA SET RUTA_CODIGO = @P1, RUTA_PRECIO_BASE_POR_KILO = @P4, RUTA_PRECIO_BASE_POR_PASAJE = @P5
 WHERE RUTA_ID = @P0
 
 if(@SERV1 is not null)
