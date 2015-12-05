c2eb9a597ff9dca19ff9262aef6deb53c7d92449
Agustin Di Prinzio <agus.diprinzio@gmail.com>
2015-12-05 07:23:26 -0300

    cambio label apellido en canje millas

--- AerolineaFrba/AerolineaFrba/Canje Millas/Canje_Millas.Designer.cs
+++ AerolineaFrba/AerolineaFrba/Canje Millas/Canje_Millas.Designer.cs
@@ -235,7 +235,7 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(47, 13);
             this.label4.TabIndex = 10;
-            this.label4.Text = "Nombre:";
+            this.label4.Text = "Apellido:";
             this.label4.Visible = false;
             // 
             // errorDniDup
